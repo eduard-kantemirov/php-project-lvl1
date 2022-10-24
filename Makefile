c4897e48cf033e6bd70c1deebda89d45e4c3752a
eduard-kantemirov <elukmanov@inbox.ru>
2022-10-24 19:48:49 +0500

    добавил игру brain-even на высчитывание четных и не четных цифр

--- Makefile
+++ Makefile
@@ -12,3 +12,6 @@ validate:
 
 lint:
 	composer exec --verbose phpcs -- --standard=PSR12 src bin
+
+brain-even:
+	./bin/brain-even
--- src/Cli.php
+++ src/Cli.php
@@ -3,9 +3,36 @@
 use function cli\line;
 use function cli\prompt;
 
-function greeting()
-{
+function greeting() {
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
+    return $name;
+}
+
+/**
+ * @throws Exception
+ */
+function even(string $name) {
+    line('Answer "yes" if the number is even, otherwise answer "no".');
+    $lose = false;
+    for ($count = 0; $count < 3; $count++) {
+        $number      = random_int(0, 9999);
+        $even        = $number % 2;
+        $rightAnswer = $even === 0 ? 'yes' : 'no';
+        $answer      = prompt('Question: ' . $number);
+
+        line("Your answer: %s", $answer);
+
+        if ($answer === $rightAnswer) {
+            line("Correct!");
+        } else {
+            line($answer . " is wrong answer ;(. Correct answer was " . $rightAnswer . ".");
+            $lose = true;
+            break;
+        }
+    }
+    if (!$lose) {
+        line("Congratulations, " . $name . "!");
+    }
 }
